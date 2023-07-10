64e0cb0db3a27e93960da158a30d7e90b8db9ef9
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2023-07-10 05:03:47 +0100

    first commit

--- script.js
+++ script.js
@@ -9,4 +9,4 @@ const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const inputElevation = document.querySelector('.form__input--elevation')
